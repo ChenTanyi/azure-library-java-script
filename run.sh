d3cf3371a5284f31c864d7f2801dfdbfab7aae1d
ChenTanyi <chentanyi@gmail.com>
2020-03-19 18:35:52 +0800

    feat: node to 13

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-wget https://nodejs.org/dist/latest-v10.x/node-v10.19.0-linux-x64.tar.gz
+wget https://nodejs.org/dist/v13.11.0/node-v13.11.0-linux-x64.tar.gz
 wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
 wget https://cdn.azul.com/zulu/bin/zulu8.44.0.11-ca-jdk8.0.242-linux_amd64.deb
 
 sudo apt install unzip
 
 unzip apache-maven-3.6.3-bin.zip
-tar zxf node-v10.19.0-linux-x64.tar.gz
+tar zxf node-v13.11.0-linux-x64.tar.gz
 sudo dpkg -i zulu8.44.0.11-ca-jdk8.0.242-linux_amd64.deb
 sudo apt install -y -f
 sudo dpkg -i zulu8.44.0.11-ca-jdk8.0.242-linux_amd64.deb
@@ -16,16 +16,17 @@ git clone https://github.com/azure/azure-libraries-for-java
 git clone https://github.com/azure/azure-rest-api-specs
 git clone https://github.com/weidongxu-microsoft/autorest.java
 
-echo 'export PATH=$PATH:~/apache-maven-3.6.3-bin/bin:~/node-v10.19.0-linux-x64/bin' >> ~/.bashrc
-export PATH=$PATH:~/apache-maven-3.6.3-bin/bin:~/node-v10.19.0-linux-x64/bin
+echo 'export PATH=$PATH:~/apache-maven-3.6.3-bin/bin:~/node-v13.11.0-linux-x64/bin' >> ~/.bashrc
+export PATH=$PATH:~/apache-maven-3.6.3-bin/bin:~/node-v13.11.0-linux-x64/bin
 
 cd autorest.java
 git checkout v4_fluentgen
 mvn package -P local
 cd ..
 
-npm install -g "@autorest/autorest"
-npm install -g gulp@3.9.1
+npm install -g autorest
+npm install -g gulp
 
 cd azure-libraries-for-java
+git checkout vnext
 npm install
