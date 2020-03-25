be2a0d8be14fd2a49e20d92ca79b2fea462eca3e
ChenTanyi <chentanyi@gmail.com>
2020-03-25 17:56:26 +0800

    feat: add windows script

---
+++ windows.sh
@@ -0,0 +1,6 @@
+curl -O https://download.jetbrains.8686c.com/idea/ideaIC-2019.3.4.exe
+curl -O https://cdn.azul.com/zulu/bin/zulu8.44.0.11-ca-jdk8.0.242-win_x64.msi
+curl -O https://mirror.bit.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
+unzip apache-maven-3.6.3-bin.zip
+
+git clone https://github.com/azure/azure-libraries-for-java
