019620123001ee55b344eeb8f8c5f8077c889d21
ChenTanyi <chentanyi@gmail.com>
2020-04-07 19:55:32 +0800

    feat: update jdk to 11

--- run.sh
+++ run.sh
@@ -2,15 +2,15 @@
 
 wget https://nodejs.org/dist/v13.11.0/node-v13.11.0-linux-x64.tar.gz
 wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
-wget https://cdn.azul.com/zulu/bin/zulu8.44.0.11-ca-jdk8.0.242-linux_amd64.deb
+wget https://cdn.azul.com/zulu/bin/zulu11.37.17-ca-jdk11.0.6-linux_amd64.deb
 
 sudo apt install unzip
 
 unzip apache-maven-3.6.3-bin.zip
 tar zxf node-v13.11.0-linux-x64.tar.gz
-sudo dpkg -i zulu8.44.0.11-ca-jdk8.0.242-linux_amd64.deb
+sudo dpkg -i zulu11.37.17-ca-jdk11.0.6-linux_amd64.deb
 sudo apt install -y -f
-sudo dpkg -i zulu8.44.0.11-ca-jdk8.0.242-linux_amd64.deb
+sudo dpkg -i zulu11.37.17-ca-jdk11.0.6-linux_amd64.deb
 
 git clone https://github.com/azure/azure-libraries-for-java
 git clone https://github.com/azure/azure-rest-api-specs
--- windows.sh
+++ windows.sh
@@ -1,5 +1,5 @@
 curl -O https://download.jetbrains.8686c.com/idea/ideaIC-2019.3.4.exe
-curl -O https://cdn.azul.com/zulu/bin/zulu8.44.0.11-ca-jdk8.0.242-win_x64.msi
+curl -O https://cdn.azul.com/zulu/bin/zulu11.37.17-ca-jdk11.0.6-win_x64.msi
 curl -O https://mirror.bit.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.zip
 unzip apache-maven-3.6.3-bin.zip
 
